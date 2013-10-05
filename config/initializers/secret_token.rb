adf56dea8a931030ccf78eae972466808086eec7
Silverballs <asilbersmith11@gmail.com>
2013-10-05 15:58:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Learnify::Application.config.secret_key_base = 'd15f7c8f321060bef91845bdbbefc32cc85187c353fa5e36167212b4d2467e60ecd6cd2d2031728d9c25f87a1c7ff74364f0829599272dc4a06cab7c4a0950f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Learnify::Application.config.session_store :cookie_store, key: '_learnify_session'
